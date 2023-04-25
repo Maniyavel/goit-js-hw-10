cee6eb7c8641838a2a29cb0b2e9ccb1012a0f089
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-04-25 22:11:47 +0300

    created fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,17 @@
+import Notiflix from 'notiflix';
+
 function fetchCountries(name){
     return fetch(
         `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flag,languages`
-    )
+    ).then((response)=>{
+        if(!response.ok){
+             throw new Error(
+                Notiflix.Notify.failure('Too many matches found. Please enter a more specific name.')
+             )
+        }
+        return response.json();
+    });
     
 }
+
+export { fetchCountries };
