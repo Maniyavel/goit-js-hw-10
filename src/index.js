809927ccd2befc3b2cbe2d57089ecc3d9e91a0e2
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-04-26 08:12:25 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './css/styles.css';
+import { fetchCountries }  from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+ const DEBOUNCE_DELAY = 300;
+ const input= document.querySelector('#search-box');
+ const list= document.querySelector('.country-list');
+ const container= document.querySelector('.country-info');
 
+input.addEventListener('input', onInput)
 
+function onInput(){
+    console.dir(evt.target);
+
+}
